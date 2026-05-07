202e220e3c841454f26b18c56c1201a657c9ca91
Natnael31 <end24002byui.edu>
2026-05-07 14:38:45 +0300

    Working with files

--- Learning/obj/Debug/net10.0/Learning.AssemblyInfo.cs
+++ Learning/obj/Debug/net10.0/Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+252300fb9a2be5ce6025aa669e34d58649692512")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
